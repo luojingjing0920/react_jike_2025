3f704937d547968cde9e2d6ec1470b6880e26a99
Tiamo <luojingjing0920@gmail.com>
2025-09-21 22:28:46 +0800

    token控制路由权限

--- react_jike/src/router/index.js
+++ react_jike/src/router/index.js
@@ -1,14 +1,15 @@
 //路由配置
 
-import Layout from "@/pages/Layout" 
+import Layout from "@/pages/Layout"
 import Login from "@/pages/Login"
 
 import { createBrowserRouter } from 'react-router-dom'
+import { AuthRoute } from '@/components/AuthRoute'
 //配置路由实例
 const router = createBrowserRouter([
     {
-        path:"/",
-        element:<Layout />
+        path: "/",
+        element: <AuthRoute><Layout /></AuthRoute>
     },
     {
         path: "/login",
