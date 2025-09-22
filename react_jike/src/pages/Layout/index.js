5146fba3f55a083990a57b9bbf17d175c2ef276b
Tiamo <luojingjing0920@gmail.com>
2025-09-22 13:46:59 +0800

    退出登录实现

--- react_jike/src/pages/Layout/index.js
+++ react_jike/src/pages/Layout/index.js
@@ -9,7 +9,8 @@ import './index.scss'
 import { Outlet, useLocation, useNavigate } from 'react-router-dom'
 import { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
-import { fetchUserInfo } from '@/store/modules/user'
+import { clearUserInfo, fetchUserInfo } from '@/store/modules/user'
+
 const { Header, Sider } = Layout
 
 const items = [
@@ -47,6 +48,12 @@ const GeekLayout = () => {
         dispatch(fetchUserInfo())
     }, [dispatch])
 
+    //退出登录确认回调
+    const onConfirm = () => {
+        dispatch(clearUserInfo())
+        navigate('/login')
+    }
+
     const name = useSelector(state => state.user.userInfo.name)
     return (
         <Layout>
@@ -55,7 +62,7 @@ const GeekLayout = () => {
                 <div className="user-info">
                     <span className="user-name">{name}</span>
                     <span className="user-logout">
-                        <Popconfirm title="是否确认退出？" okText="退出" cancelText="取消">
+                        <Popconfirm title="是否确认退出？" okText="退出" cancelText="取消" onConfirm={onConfirm}>
                             <LogoutOutlined /> 退出
                         </Popconfirm>
                     </span>
--- react_jike/src/store/modules/user.js
+++ react_jike/src/store/modules/user.js
@@ -1,7 +1,7 @@
 //和用户相关的状态管理
 
 import { createSlice } from '@reduxjs/toolkit'
-import { request } from '@/utils'
+import { removeToken, request } from '@/utils'
 import {setToken as _setToken,getToken} from '@/utils'
 
 const userStore = createSlice({
@@ -20,13 +20,18 @@ const userStore = createSlice({
         },
         setUserInfo(state,action) {
             state.userInfo = action.payload
+        },
+        clearUserInfo(state){
+            state.token = ''
+            state.userInfo = {}
+            removeToken()
         }
     }
 })
 
 //解构actionCreater
 
-const { setToken,setUserInfo } = userStore.actions
+const { setToken, setUserInfo, clearUserInfo } = userStore.actions
 
 //获取reducer函数
 const userReducer = userStore.reducer
@@ -50,5 +55,5 @@ const fetchUserInfo = () => {
 }
 
 
-export { fetchLogin, fetchUserInfo, setToken }
+export { fetchLogin, fetchUserInfo, setToken, clearUserInfo }
 export default userReducer
