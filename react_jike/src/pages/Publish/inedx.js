0fe3f4d72fd836be8f918c3eac00fb05bec85424
Tiamo <luojingjing0920@gmail.com>
2025-09-23 12:50:50 +0800

    图片封面类型的切换交互实现

--- react_jike/src/pages/Publish/inedx.js
+++ react_jike/src/pages/Publish/inedx.js
@@ -56,6 +56,12 @@ const Publish = () => {
     const onChange=(value)=>{
         setimageList(value.fileList)
     }
+
+    //切换图片封面类型
+    const [imageType,setImageType] = useState(0)
+    const onTypeChange = (e)=>{
+        setImageType(e.target.value)
+    }
     return (
         <div className="publish">
             <Card
@@ -70,7 +76,7 @@ const Publish = () => {
                 <Form
                     labelCol={{ span: 4 }}
                     wrapperCol={{ span: 16 }}
-                    initialValues={{ type: 1 }}
+                    initialValues={{ type: 0 }}
                     onFinish={onFinish}
                 >
                     <Form.Item
@@ -93,7 +99,7 @@ const Publish = () => {
 
                     <Form.Item label="封面">
                         <Form.Item name="type">
-                            <Radio.Group>
+                            <Radio.Group onChange={onTypeChange}>
                                 <Radio value={1}>单图</Radio>
                                 <Radio value={3}>三图</Radio>
                                 <Radio value={0}>无图</Radio>
@@ -103,7 +109,7 @@ const Publish = () => {
                             listType:决定选择文件框的外观样式
                             showUploadList:控制显示上传列表
                         */}
-                        <Upload
+                        {imageType > 0 && <Upload
                             listType="picture-card"
                             showUploadList
                             action={'http://geek.itheima.net/v1_0/upload'}
@@ -113,7 +119,8 @@ const Publish = () => {
                             <div style={{ marginTop: 8 }}>
                                 <PlusOutlined />
                             </div>
-                        </Upload>
+                        </Upload>}
+                        
                     </Form.Item>
 
                     <Form.Item
