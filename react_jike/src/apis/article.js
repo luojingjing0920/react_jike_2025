f7bb5c11c0d43095d8cdff71732c45081039dec0
Tiamo <luojingjing0920@gmail.com>
2025-09-23 21:22:16 +0800

    文章列表-基础table的渲染

--- react_jike/src/apis/article.js
+++ react_jike/src/apis/article.js
@@ -19,3 +19,12 @@ export function createArticleAPI(data){
         data
     })
 }
+
+//获取文章列表
+export function getArticleListAPI(params){
+    return request({
+        url:'/mp/articles',
+        method:'GET',
+        params
+    })
+}
--- react_jike/src/pages/Article/index.js
+++ react_jike/src/pages/Article/index.js
@@ -8,6 +8,8 @@ import { Table, Tag, Space } from 'antd'
 import { EditOutlined, DeleteOutlined } from '@ant-design/icons'
 import img404 from '@/assets/error.png'
 import { useChannel } from '@/hook/useChannel'
+import { useEffect, useState } from 'react'
+import { getArticleListAPI } from '@/apis/article'
 
 
 const { Option } = Select
@@ -83,6 +85,18 @@ const Article = () => {
             title: 'wkwebview离线化加载h5资源解决方案'
         }
     ]
+
+    //获取文章列表
+    const [list,setList] =useState([])
+    const [count,setCount] = useState(0)
+    useEffect(()=>{
+        async function getList(){
+           const res = await getArticleListAPI()
+            setList(res.data.results)
+            setCount(res.data.total_count)
+        }
+        getList()
+    },[])
     return (
         <div>
             <Card
@@ -128,8 +142,8 @@ const Article = () => {
                 </Form>
             </Card>
             {/* 表格区域 */}
-            <Card title={`根据筛选条件共查询到 count 条结果：`}>
-                <Table rowKey="id" columns={columns} dataSource={data} />
+            <Card title={`根据筛选条件共查询到 ${count} 条结果：`}>
+                <Table rowKey="id" columns={columns} dataSource={list} />
             </Card>
         </div>
     )
