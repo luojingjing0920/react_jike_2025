47f2d82e6e063d6b0cee30b2073025ec53c0758b
Tiamo <luojingjing0920@gmail.com>
2025-09-24 18:09:44 +0800

    打包路由懒加载优化

--- react_jike/src/router/index.js
+++ react_jike/src/router/index.js
@@ -5,25 +5,32 @@ import Login from "@/pages/Login"
 
 import { createBrowserRouter } from 'react-router-dom'
 import { AuthRoute } from '@/components/AuthRoute'
-import Home from "@/pages/Home"
-import Article from "@/pages/Article"
-import Publish from "@/pages/Publish/inedx"
+import { lazy, Suspense } from 'react'
+// import Home from "@/pages/Home"
+// import Article from "@/pages/Article"
+// import Publish from "@/pages/Publish/inedx"
+
+//1.lazy函数对组件进行导入
+const Home = lazy(() => import("@/pages/Home"))
+const Article = lazy(() => import("@/pages/Article"))
+const Publish = lazy(() => import("@/pages/Publish/inedx"))
+
 //配置路由实例
 const router = createBrowserRouter([
     {
         path: "/",
         element: <AuthRoute><Layout /></AuthRoute>,
-        children:[{
-            path:'/',
-            element: <AuthRoute><Home /></AuthRoute>
+        children: [{
+            path: '/',
+            element: <Suspense fallback={'加载中'}><Home /></Suspense>
         },
         {
-            path:'/article',
-            element:<Article />
+            path: '/article',
+            element: <Suspense fallback={'加载中'}><Article /></Suspense>
         },
         {
-            path:'publish',
-            element:<Publish />
+            path: 'publish',
+            element: <Suspense fallback={'加载中'}><Publish /></Suspense>
         }]
     },
     {
